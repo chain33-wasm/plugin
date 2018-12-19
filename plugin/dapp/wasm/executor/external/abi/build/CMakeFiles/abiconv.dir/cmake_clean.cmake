353c6c5871adc50dd635a1dc925c5a2842ec752b
yann <yxq@disanbo.com>
2018-12-19 09:55:04 +0800

    test

---
+++ plugin/dapp/wasm/executor/external/abi/build/CMakeFiles/abiconv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abiconv.dir/src/abieos.cpp.o"
+  "libabiconv.pdb"
+  "libabiconv.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abiconv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
